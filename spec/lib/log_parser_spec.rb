99db6164dd348ae9787346ab2588203ff422c4da
Debanjan Sengupta <dave4u90@gmail.com>
2021-08-04 01:43:50 +0530

    RSpec for grouping the parsed data by different criteria and extracting count from each group

--- spec/lib/log_parser_spec.rb
+++ spec/lib/log_parser_spec.rb
@@ -51,4 +51,61 @@ describe LogParser do
       expect(subject.all? { |e| e.keys == [:endpoint, :controller_path, :user_addr] }).to be_truthy
     end
   end
+
+  #[{ endpoint: '/home/2', entry_count: 10 }]
+  xdescribe '#entry_counts_by_endpoint' do
+    before { parser.parse }
+
+    subject { parser.entry_counts_by_endpoint }
+    
+    it 'should return an array' do
+      expect(subject.class).to eq Array
+    end
+
+    it 'should have a hash as each element in the array' do
+      expect(subject.all? { |e| e.is_a? Hash }).to be_truthy
+    end
+
+    it 'should have expected keys in each hashes in the array' do
+      expect(subject.all? { |e| e.keys == [:endpoint, :entry_count] }).to be_truthy
+    end
+  end
+
+  #[{ endpoint: '/home', entry_count: 15 }]
+  xdescribe '#entry_counts_by_controller_path' do
+    before { parser.parse }
+
+    subject { parser.entry_counts_by_controller_path }
+    
+    it 'should return an array' do
+      expect(subject.class).to eq Array
+    end
+
+    it 'should have a hash as each element in the array' do
+      expect(subject.all? { |e| e.is_a? Hash }).to be_truthy
+    end
+
+    it 'should have expected keys in each hashes in the array' do
+      expect(subject.all? { |e| e.keys == [:controller_path, :entry_count] }).to be_truthy
+    end
+  end
+
+  #[{ user_addr: '8.8.8.8', entry_count: 100 }]
+  xdescribe '#entry_counts_by_user_addr' do
+    before { parser.parse }
+
+    subject { parser.entry_counts_by_user_addr }
+    
+    it 'should return an array' do
+      expect(subject.class).to eq Array
+    end
+
+    it 'should have a hash as each element in the array' do
+      expect(subject.all? { |e| e.is_a? Hash }).to be_truthy
+    end
+
+    it 'should have expected keys in each hashes in the array' do
+      expect(subject.all? { |e| e.keys == [:user_addr, :entry_count] }).to be_truthy
+    end
+  end
 end
