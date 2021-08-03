34401e315966dd1586df1936d3173701ddbe9c22
Debanjan Sengupta <dave4u90@gmail.com>
2021-08-04 01:54:18 +0530

    RSpec for sorting the grouped data and making sure all properties are retained

--- spec/lib/log_parser_spec.rb
+++ spec/lib/log_parser_spec.rb
@@ -108,4 +108,113 @@ describe LogParser do
       expect(subject.all? { |e| e.keys == [:user_addr, :entry_count] }).to be_truthy
     end
   end
+
+  xdescribe '#sorted_endpoint_entries' do
+    before do
+      allow(parser).to receive(:entry_counts_by_endpoint).and_return(
+        [
+          { endpoint: 'endpoint1', entry_count: 50 },
+          { endpoint: 'endpoint2', entry_count: 100},
+          { endpoint: 'endpoint3', entry_count: 60 }
+        ]
+      )
+    end
+
+    subject { parser.sorted_endpoint_entries }
+
+    it 'should return an array' do
+      expect(subject.class).to eq Array
+    end
+
+    it 'should have a hash as each element in the array' do
+      expect(subject.all? { |e| e.is_a? Hash }).to be_truthy
+    end
+
+    it 'should have expected keys in each hashes in the array' do
+      expect(subject.all? { |e| e.keys == [:endpoint, :entry_count] }).to be_truthy
+    end
+
+    it 'should sort the entries by no of count in descending order' do
+      expect(subject).to eq(
+        [
+          { endpoint: 'endpoint2', entry_count: 100},
+          { endpoint: 'endpoint3', entry_count: 60 },
+          { endpoint: 'endpoint1', entry_count: 50 }
+        ]
+      )
+    end
+  end
+
+  xdescribe '#sorted_controller_path_entries' do
+    before do
+      allow(parser).to receive(:entry_counts_by_controller_path).and_return(
+        [
+          { controller_path: 'controller1', entry_count: 50 },
+          { controller_path: 'controller2', entry_count: 100},
+          { controller_path: 'controller3', entry_count: 60 }
+        ]
+      )
+    end
+
+    subject { parser.sorted_controller_path_entries }
+
+    it 'should return an array' do
+      expect(subject.class).to eq Array
+    end
+
+    it 'should have a hash as each element in the array' do
+      expect(subject.all? { |e| e.is_a? Hash }).to be_truthy
+    end
+
+    it 'should have expected keys in each hashes in the array' do
+      expect(subject.all? { |e| e.keys == [:controller_path, :entry_count] }).to be_truthy
+    end
+
+    it 'should sort the entries by no of count in descending order' do
+      expect(subject).to eq(
+        [
+          { controller_path: 'controller2', entry_count: 100},
+          { controller_path: 'controller3', entry_count: 60 },
+          { controller_path: 'controller1', entry_count: 50 }
+        ]
+      )
+    end
+  end
+
+  xdescribe '#sorted_user_addr_entries' do
+    before do
+      allow(parser).to receive(:entry_counts_by_user_addr).and_return(
+        [
+          { user_addr: '1.1.1.1', entry_count: 50 },
+          { user_addr: '2.2.2.2', entry_count: 100 },
+          { user_addr: '3.3.3.3', entry_count: 60 },
+          { user_addr: '4.4.4.4', entry_count: 200 }
+        ]
+      )
+    end
+
+    subject { parser.sorted_user_addr_entries }
+
+    it 'should return an array' do
+      expect(subject.class).to eq Array
+    end
+
+    it 'should have a hash as each element in the array' do
+      expect(subject.all? { |e| e.is_a? Hash }).to be_truthy
+    end
+
+    it 'should have expected keys in each hashes in the array' do
+      expect(subject.all? { |e| e.keys == [:user_addr, :entry_count] }).to be_truthy
+    end
+
+    it 'should sort the entries by no of count in descending order and return top three' do
+      expect(subject).to eq(
+        [
+          { user_addr: '4.4.4.4', entry_count: 200 },
+          { user_addr: '2.2.2.2', entry_count: 100 },
+          { user_addr: '3.3.3.3', entry_count: 60 }
+        ]
+      )
+    end
+  end
 end
