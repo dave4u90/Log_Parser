49d158bb71cd3e268efd3b274d62d2e47eaa8523
Debanjan Sengupta <dave4u90@gmail.com>
2021-08-04 02:41:29 +0530

    Exposed the statistics gathered in terminal via a launcher script

--- parser.rb
+++ parser.rb
@@ -1,5 +1,33 @@
 #!/usr/bin/env ruby
-
+require 'pry'
+require 'colorize'
 require_relative 'lib/log_parser'
 
 parser = LogParser.new(logfile: ARGV[0])
+
+if parser.valid?
+  parser.parse
+  puts "Visit Summary".bold.yellow
+
+  parser.sorted_endpoint_entries.each do |visit_data|
+    puts visit_data[:endpoint].green + " " + "#{visit_data[:entry_count]} visits".light_blue
+  end
+
+  puts ""
+
+  puts "Unique Visits Summary".bold.yellow
+
+  parser.sorted_controller_path_entries.each do |visit_data|
+    puts visit_data[:controller_path].green + " " + "#{visit_data[:entry_count]} visits".light_blue
+  end
+
+  puts ""
+
+  puts "Most Frequent Visitors".bold.yellow
+
+  parser.sorted_user_addr_entries.each do |visit_data|
+    puts visit_data[:user_addr].green + " " + "#{visit_data[:entry_count]} visits".light_blue
+  end
+else
+  puts parser.errors.red
+end
