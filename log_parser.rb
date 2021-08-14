bae7402c6266f5319556e3c6fee337fd22d766f3
Debanjan Sengupta <dave4u90@gmail.com>
2021-08-15 00:53:29 +0530

    Encapsulate all required statement

---
+++ log_parser.rb
@@ -0,0 +1,7 @@
+require_relative 'lib/log_parser/parser'
+require_relative 'lib/log_parser/log_entry'
+require_relative 'lib/log_parser/log_entry_group'
+require_relative 'lib/log_parser/log_entry_groups'
+
+module LogParser
+end
